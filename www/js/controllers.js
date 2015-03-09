2bf5e500ddd8cf7aeccd09c3c3c105bdb2871401
clouie87 <carol.louie@gmail.com>
2015-03-09 14:17:09 -0300

    adding in the camera functionality

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,9 +13,30 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('CameraCtrl', function($scope){
-  })
+  .controller('CameraCtrl', function($scope, $state) {
 
+    // set the default image URI (a color bar image)
+    $scope.imageURI = 'http://www.arabamerica.com/wp-content/themes/arabamerica/assets/img/thumbnail-default.jpg';
+
+    // I want to go directly here from camera button... need to think about this!
+    $scope.takePhoto = function() {
+
+      var cameraOptions = {
+        targetWidth: 300,
+        targetHeight: 300
+      };
+
+      navigator.camera.getPicture(function(imageURI) {
+
+        $scope.imageURI = imageURI;
+        $state.go('tab.camera');
+
+      }, function(err) {
+
+        alert("Sorry!  Can't take your photo!");
+      }, cameraOptions);
+    }
+  })
 .controller('WinsCtrl', function($scope){
   })
 
